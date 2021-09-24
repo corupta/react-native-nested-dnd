82a454982fd7ea46fcb659d29d9cfd4d5c2ebf5f
corupta <halitozsoy1584@gmail.com>
2021-09-24 05:35:40 +0300

    updates for reanimated,  update core

--- examples/babel.config.js
+++ examples/babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'react-native-reanimated/plugin',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
